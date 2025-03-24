a0c71e87a077d3489c55e20eab52678a2976eaad
Gabriel Sena <gabrielsenasaraiva7@gmail.com>
2025-03-24 02:12:52 -0300

    change api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:350"
 // export const API_URL = import.meta.env.NODE_ENV == "prod" ? import.meta.env.VITE_API_URL : import.meta.env.VITE_LOCAL_URL
-export const API_URL = import.meta.env.VITE_LOCAL_URL
+export const API_URL = import.meta.env.VITE_API_URL
